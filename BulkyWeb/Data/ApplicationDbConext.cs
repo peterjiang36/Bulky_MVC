ef68884b62e072deb15cafdadb01718e32a91f46
Peter Jiang <peter.j@usask.ca>
2025-05-29 11:26:14 -0600

    set up connection string

---
+++ BulkyWeb/Data/ApplicationDbConext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbConext : DbContext
+    {
+        public ApplicationDbConext(DbContextOptions<ApplicationDbConext> options) : base(options)
+        {
+
+        }
+    }
+}
