766dde9edcffea4465ba6812bb81181a5811a399
Peter Jiang <peter.j@usask.ca>
2025-06-06 11:33:17 -0600

    implemented ViewData instead of ViewBag

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -30,7 +30,9 @@ namespace BulkyWeb.Areas.Admin.Controllers
                     Value = u.Id.ToString()
                 });
 
-            ViewBag.CategoryList = CategoryList;
+            //ViewBag.CategoryList = CategoryList;
+            ViewData["CategoryList"] = CategoryList;
+
             return View();
         }
 
