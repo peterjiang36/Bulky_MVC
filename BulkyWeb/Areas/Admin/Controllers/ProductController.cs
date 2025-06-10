6734ce638e6468b002a1662889c6bb52d43f14d0
Peter Jiang <peter.j@usask.ca>
2025-06-10 14:42:23 -0600

    handle image on update

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -63,6 +63,17 @@ namespace BulkyWeb.Areas.Admin.Controllers
                     string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
                     string productPath = Path.Combine(wwwRootPath, @"images\product");
 
+                    if (!string.IsNullOrEmpty(productVM.Product.ImageUrl))
+                    {
+                        // delete the old image
+                        var oldImagePath = Path.Combine(wwwRootPath, productVM.Product.ImageUrl.TrimStart('\\'));
+
+                        if (System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+
                     using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
                     {
                         file.CopyTo(fileStream);
@@ -71,7 +82,14 @@ namespace BulkyWeb.Areas.Admin.Controllers
                     productVM.Product.ImageUrl = @"\images\product\" + fileName;
                 }
 
-                _unitOfWork.Product.Add(productVM.Product); //add the new Product object to the Product table
+                if (productVM.Product.Id == 0)
+                {
+                    _unitOfWork.Product.Add(productVM.Product); //add the new Product object to the Product table
+                } else
+                {
+                    _unitOfWork.Product.Update(productVM.Product); //add the new Product object to the Product table
+
+                }
                 _unitOfWork.Save(); // go to database to create the product
                 TempData["success"] = "Product created successfully";
                 return RedirectToAction("Index"); //reload the Categry and pass it the view
