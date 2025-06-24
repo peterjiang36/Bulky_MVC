f02d8e3636b3926e248cdd3e350c80e78c827058
Peter Jiang <peter.j@usask.ca>
2025-06-24 16:09:28 -0600

    added shopping cart repository

--- Bulky.DataAccess/Repository/UnitOfWork.cs
+++ Bulky.DataAccess/Repository/UnitOfWork.cs
@@ -13,12 +13,13 @@ namespace Bulky.DataAccess.Repository
         private ApplicationDbContext _db;
         public ICategoryRepository Category { get; private set; }
         public ICompanyRepository Company { get; private set; }
-
         public IProductRepository Product { get; private set; }
+        public IShoppingCartRepository ShoppingCart { get; set; }
 
         public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
+            ShoppingCart = new ShoppingCartRepository(_db);
             Category = new CategoryRepository(_db);
             Product = new ProductRepository(_db);
             Company = new CompanyRepository(_db);
