9b446652c989f933201ac44676ee2c3a47c7dec1
Peter Jiang <peter.j@usask.ca>
2025-06-25 11:53:19 -0600

    updated ShoppingCartVM more dynamic

---
+++ Bulky.DataAccess/Repository/OrderHeaderRepository.cs
@@ -0,0 +1,25 @@
+﻿using Bulky.DataAcess.Data;
+using Bulky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository.IRepository
+{
+    public class OrderHeaderRepository : Repository<OrderHeader>, IOrderHeaderRepository
+    {
+        private ApplicationDbContext _db;
+        public OrderHeaderRepository(ApplicationDbContext db): base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(OrderHeader obj)
+        {
+            _db.OrderHeaders.Update(obj);
+        }
+    }
+}
