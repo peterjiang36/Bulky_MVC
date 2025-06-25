5f6cb7f1a90eb1b328e9dc706f4a2030df1fae8d
Peter Jiang <peter.j@usask.ca>
2025-06-25 10:36:17 -0600

    get shopping cart

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -1,17 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace BulkyWeb.Areas.Customer.Controllers
 {
     [Area("customer")]
+    [Authorize]
     public class CartController : Controller
     {
+        private readonly IUnitofWork _unitOfWork;
+        public ShoppingCartVM ShoppingCartVM { get; set; }
+        public CartController (IUnitofWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        } 
+
+
         public IActionResult Index()
         {
-            return View();
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            ShoppingCartVM = new()
+            {
+                ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId,
+                includeProperties: "Product")
+            };
+            return View(ShoppingCartVM);
         }
     }
 }
